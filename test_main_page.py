0f1c57de0e0f82f31ce8339d565f3501aded14ac
Mykola Povshednyi <povshednick@gmail.com>
2022-08-02 15:11:05 +0300

    test files linked

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,17 @@
+from .pages.main_page import MainPage
 import time
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    time.sleep(5)
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    time.sleep(5)
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    time.sleep(5)
