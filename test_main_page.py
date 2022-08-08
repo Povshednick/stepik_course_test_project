a685ec2adc0be924b8056e452f99c7c278f895dc
Mykola Povshednyi <povshednick@gmail.com>
2022-08-08 10:19:40 +0300

    page initializing in test

--- test_main_page.py
+++ test_main_page.py
@@ -9,22 +9,19 @@ def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
 
-def guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-def guest_should_see_login_link(browser):
+def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
-def test_guest_should_open_login_link(browser):
+def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    time.sleep(3)
-    page = LoginPage(browser, link)
-    page.should_be_login_url()
-    page.should_be_login_form()
-    page.should_be_register_form()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
