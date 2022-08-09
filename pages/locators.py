7ec50da81092cb1665961832ffff2efe57b43c99
Mykola Povshednyi <povshednick@gmail.com>
2022-08-09 13:42:38 +0300

    change logic of login link

--- pages/locators.py
+++ pages/locators.py
@@ -13,3 +13,8 @@ class ProductPageLocators():
     PRODUCT_PRICE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
     PRODUCT_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
     CART_TOTAL = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div:nth-child(1)")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -16,3 +16,9 @@ class ProductPage(BasePage):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         total_price = self.browser.find_element(*ProductPageLocators.CART_TOTAL).text
         assert product_price == total_price, "Total not match"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
+    def should_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should disappeared"
